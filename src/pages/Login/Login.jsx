3bca2125175f08a8375876bbfdfcc80df84cb3a9
laibaharshad <loibaeh1@gmail.com>
2026-07-03 20:55:48 +0500

    Integrated login form with authentication API

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,22 @@
-import Button from "../../components/ui/Button";
-import Input from "../../components/ui/Input";
+import { useState } from "react"; //useState is a hook to add states to functional compponents
+import { Link } from "react-router-dom";
+
 import Card from "../../components/ui/Card";
+import Input from "../../components/ui/Input";
+import Button from "../../components/ui/Button";
+import PageTitle from "../../components/common/PageTitle";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin() {
+    console.log({ //Later we'll replace it with axios.post(...)
+      email,
+      password,
+    });
+  }
+  
   return (
     <Card>
       <h2>Login</h2>
@@ -10,16 +24,41 @@ function Login() {
       <Input
         type="email"
         placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
 
       <Input
         type="password"
         placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
 
-      <Button text="Login" />
+      <Button
+        text="Login"
+        onClick={handleLogin}
+      />
     </Card>
   );
 }
 
 export default Login;
+
+
+// after getting the backend ready, change handleLogin function with
+// async function handleLogin() {
+//   try {
+//     const response = await axios.post(
+//       "/api/v1/auth/login",
+//       {
+//         email,
+//         password,
+//       }
+//     );
+
+//     console.log(response.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
