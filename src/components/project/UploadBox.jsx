0e8ca8d4cfb59b26575bac82fb2faaab13b19e35
Hafisa Khaliq <hafisakhaliq00@gmail.com>
2026-07-07 00:46:43 +0500

    Day 1: Setup Hafsa module structure

---
+++ src/components/project/UploadBox.jsx
@@ -0,0 +1,5 @@
+function UploadBox() {
+  return <div>Upload Box</div>;
+}
+
+export default UploadBox;
---
+++ src/pages/ProjectRecommendation/ProjectRecommendation.jsx
@@ -0,0 +1,9 @@
+function ProjectRecommendation() {
+  return (
+    <div>
+      <h1>Project Recommendation</h1>
+    </div>
+  );
+}
+
+export default ProjectRecommendation;
